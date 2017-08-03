58b7bc8157c65c64e30c62ec0eb61f018b34df51
mgdreamingstar <mgdreamingstar>
2017-08-03 19:06:27 +0800

    Undistortion

--- Correct-for-distortion.py
+++ Correct-for-distortion.py
@@ -3,32 +3,48 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import os, glob
+%matplotlib inline
+# # Read in the saved objpoints and imgpoints
+# dist_pickle = pickle.load( open( "wide_dist_pickle.p", "rb" ) )
+# objpoints = dist_pickle["objpoints"]
+# imgpoints = dist_pickle["imgpoints"]
 
-# Read in the saved objpoints and imgpoints
-dist_pickle = pickle.load( open( "wide_dist_pickle.p", "rb" ) )
-objpoints = dist_pickle["objpoints"]
-imgpoints = dist_pickle["imgpoints"]
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
-# Read in an image
-img = cv2.imread('test_image.png')
+objpoints = []
+imgpoints = []
 
-# TODO: Write a function that takes an image, object points, and image points
-# performs the camera calibration, image distortion correction and
-# returns the undistorted image
-def cal_undistort(img, objpoints, imgpoints):
-    # Use cv2.calibrateCamera() and cv2.undistort()
-    # ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    img_size = (img.shape[1], img.shape[0])
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
-    undist = cv2.undistort(img,mtx,dist,None,mtx)
-    return undist
+camera_cal = os.path.abspath('D:\Github\CarND-Advanced-Lane-Lines\camera_cal')
+imglist = glob.glob(camera_cal + '\*.jpg')
 
-undistorted = cal_undistort(img, objpoints, imgpoints)
+for idx, imgname in enumerate(imglist):
+    img = mpimg.imread(imgname)
+    gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY) # convert to grayscale picture
 
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-f.tight_layout()
-ax1.imshow(img)
-ax1.set_title('Original Image', fontsize=50)
-ax2.imshow(undistorted)
-ax2.set_title('Undistorted Image', fontsize=50)
-plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+    ret, corners = cv2.findChessboardCorners(gray, (9,6), None) # find imgpoints
+
+    if ret == True:
+        imgpoints.append(corners)
+        objpoints.append(objp)
+
+        img = cv2.drawChessboardCorners(img, (9,6), corners, ret) # draw imgpoints on image
+        img_size = (img.shape[1], img.shape[0])
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+        undist = cv2.undistort(img,mtx,dist,None,mtx)
+        cv2.imshow('Undistorted', undist)
+        cv2.waitKey(500)
+
+cv2.destroyAllWindows()
+
+
+# undistorted = cal_undistort(img, objpoints, imgpoints)
+#
+# f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+# f.tight_layout()
+# ax1.imshow(img)
+# ax1.set_title('Original Image', fontsize=50)
+# ax2.imshow(undistorted)
+# ax2.set_title('Undistorted Image', fontsize=50)
+# plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
